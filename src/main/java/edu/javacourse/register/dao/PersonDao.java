b589aae0a3ee0c83cfeadd6ea8eab66ef2a53da4
Andrey <xramovic@yandex.ru>
2021-08-08 12:42:03 +0600

    119

--- src/main/java/edu/javacourse/register/dao/PersonDao.java
+++ src/main/java/edu/javacourse/register/dao/PersonDao.java
@@ -1,8 +1,11 @@
 package edu.javacourse.register.dao;
 
+import edu.javacourse.register.domain.Person;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.List;
 
 public class PersonDao {
@@ -14,7 +17,9 @@ public class PersonDao {
         entityManager = factory.createEntityManager();
     }
 
-    public List findPersons() {
-        return entityManager.createQuery("Select p from Person p").getResultList();
+    public List<Person> findPersons() {
+        Query query = entityManager.createNamedQuery("Person.findPersons");
+        query.setParameter("Person", 1L);
+        return query.getResultList();
     }
 }
