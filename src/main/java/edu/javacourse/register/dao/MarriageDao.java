3ba4f1c98bfc6b1a21b908f17df17127ff1cbc10
Andrey <xramovic@yandex.ru>
2021-08-09 21:08:22 +0600

    Знакомство со Спрингом

--- src/main/java/edu/javacourse/register/dao/MarriageDao.java
+++ src/main/java/edu/javacourse/register/dao/MarriageDao.java
@@ -4,19 +4,22 @@ import edu.javacourse.register.domain.MarriageCertificate;
 import edu.javacourse.register.view.MarriageRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 
+@Component
 public class MarriageDao {
 
+    @Autowired
     private static final Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
 
     private EntityManager entityManager;
+    @Value("${test.value}")
     private String test;
 
-    public void setTest(String test) {
-        this.test = test;
-    }
 
     public MarriageCertificate findMarriageCertificate(MarriageRequest request) {
         LOGGER.info("findMarriageCertificate called:{}",test);
