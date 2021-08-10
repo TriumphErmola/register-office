832028e9888089845e069d597e3f209007f0d7c2
Andrey <xramovic@yandex.ru>
2021-08-10 20:51:43 +0600

    Знакомство со Спрингом

--- src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -8,9 +8,12 @@ import edu.javacourse.register.view.MarriageResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("marriageService")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class MarriageManager {
 
 
@@ -21,13 +24,11 @@ public class MarriageManager {
     @Autowired
     private PersonDao personDao;
 
-    public MarriageManager(PersonDao personDao) {
-        this.personDao = personDao;
-    }
 
     public MarriageResponse findMarriageCertificate(MarriageRequest request) {
         LOGGER.info("findMarriageCertificate called");
         MarriageCertificate cert = marriageDao.findMarriageCertificate(request);
+        personDao.findPersons();
         return new MarriageResponse();
     }
 }
